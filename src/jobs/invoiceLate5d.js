1a9dbd3733bab2a18ec47c78c6a05f34917d19b8
domems <domems@gmail.com>
2025-10-24 17:33:53 +0100

    Notifications

--- src/jobs/invoiceLate5d.js
+++ src/jobs/invoiceLate5d.js
@@ -55,7 +55,7 @@ export async function runInvoiceLate5dOnce() {
 export function startInvoiceLate5dLoop() {
   // Todos os dias às 17:30 hora de Lisboa
   cron.schedule(
-    "30 17 * * *",
+    "35 17 * * *",
     async () => {
       try {
         const r = await runInvoiceLate5dOnce();
